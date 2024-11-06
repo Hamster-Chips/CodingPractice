baa3a1ff57635c45688a19bc79853e070b22400e
JorgeM1110 <mejiapity404@gmail.com>
2024-11-06 09:13:08 -0800

    set up

---
+++ practice.py
@@ -0,0 +1,54 @@
+'''
+Imagine a black and white image and represts by pixel, pixels are black or white
+
+2d Array
+0, 1
+1 -> Black
+0 -> White
+
+remove all black pixel that are not connected to the boarded of the image
+- up/down/left/right
+- No diagonal
+
+'''
+
+def removeIsland(matrix):
+    
+    '''
+    Do a nested loop to go through the matrix
+    Check the first elem if 1 or not
+    Check the 2nd elem if there is a 1 or not
+        - If there is a 1
+            - Look if the first elem was a 1
+                - if first elem is a 1 
+                    - Store it in a 'hold' variable <- index
+                - else
+                    - Store it in the 'delete' variable  <- index
+    '''
+
+
+
+def main():
+
+    matrix = [
+        [0,1,0,0,1,0,1,1,1]
+
+
+
+
+
+
+
+    ]
+
+
+
+
+
+
+
+
+
+
+
+    main()
